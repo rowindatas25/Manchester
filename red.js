76151a32fe89493cf06ac3d5dc5971b5c0c1778a
Rohtas Wadera <rohtaswadera@gmail.com>
2018-07-05 10:23:12 -0400

    push notifications permission added

--- red.js
+++ red.js
@@ -1,6 +1,13 @@
+var deferredPrompt;
+var enableNotifications = document.querySelectorAll('.notifications');
+
+if (!window.Promise) {
+	window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/Manchester/sw.js')
+        .register('/sw.js')
         .then(function() {
             console.log('Service Worker registered');
         })
@@ -16,6 +23,31 @@ function displayCard() {
 window.addEventListener('beforeinstallprompt', function(event) {
     console.log('Prompt is running!');
     event.preventDefault();
+    deferredPrompt = event;
     return false;
 });
 
+function confirmNotification() {
+	new Notification('Successfully Subscribed!');
+}
+
+function permission() {
+	Notification.requestPermission(function(result) {
+		console.log('User Choice', result);
+		if (result !== 'granted') {
+			console.log('Permission was not granted!');
+		} else {
+			confirmNotification();
+		}
+	});
+}
+
+
+if ('Notification' in window) {
+	for (var i = 0; i < enableNotifications.length; i++) {
+		enableNotifications[i].style.display = 'inline-block';
+		enableNotifications[i].style.margin = '0 auto';
+		enableNotifications[i].addEventListener("click", permission);
+		console.log('Button is working');
+	}
+}
